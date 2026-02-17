50b7d3fce6f7fa819ae1c31d7c6d42c05d43e01c
Vandan Agrawal <vandan28agrawal@gmail.com>
2026-02-16 19:40:08 -0600

    implementation of ingredient FOPC

---
+++ FoodData_Central_foundation_food_csv_2025-12-18/facts_nutrients.pl
@@ -0,0 +1,305 @@
+% Nutrient facts per 100g
+HasNutrient(almonds, calories).
+NutrientValuePer100g(almonds, calories, 1605).
+HasNutrient(almonds, carbs).
+NutrientValuePer100g(almonds, carbs, 18.1173).
+HasNutrient(almonds, fat).
+NutrientValuePer100g(almonds, fat, 54.445).
+HasNutrient(almonds, protein).
+NutrientValuePer100g(almonds, protein, 20.9252).
+HasNutrient(apricot, carbs).
+NutrientValuePer100g(apricot, carbs, 10.2387).
+HasNutrient(apricot, fat).
+NutrientValuePer100g(apricot, fat, 0.405).
+HasNutrient(apricot, protein).
+NutrientValuePer100g(apricot, protein, 0.96125).
+HasNutrient(avocado, carbs).
+NutrientValuePer100g(avocado, carbs, 8.3235).
+HasNutrient(avocado, fat).
+NutrientValuePer100g(avocado, fat, 20.31).
+HasNutrient(avocado, protein).
+NutrientValuePer100g(avocado, protein, 1.8125).
+HasNutrient(banana, calories).
+NutrientValuePer100g(banana, calories, 236.75).
+HasNutrient(banana, carbs).
+NutrientValuePer100g(banana, carbs, 18.2332).
+HasNutrient(banana, fat).
+NutrientValuePer100g(banana, fat, 0.23028).
+HasNutrient(banana, protein).
+NutrientValuePer100g(banana, protein, 0.732625).
+HasNutrient(beans, calories).
+NutrientValuePer100g(beans, calories, 323.75).
+HasNutrient(beans, carbs).
+NutrientValuePer100g(beans, carbs, 24.8859).
+HasNutrient(beans, fat).
+NutrientValuePer100g(beans, fat, 1.52917).
+HasNutrient(beans, protein).
+NutrientValuePer100g(beans, protein, 19.9996).
+HasNutrient(beef, calories).
+NutrientValuePer100g(beef, calories, 492.2).
+HasNutrient(beef, carbs).
+NutrientValuePer100g(beef, carbs, 0.46256).
+HasNutrient(beef, fat).
+NutrientValuePer100g(beef, fat, 10.9607).
+HasNutrient(beef, protein).
+NutrientValuePer100g(beef, protein, 21.3471).
+HasNutrient(bison, carbs).
+NutrientValuePer100g(bison, carbs, -0.14955).
+HasNutrient(bison, fat).
+NutrientValuePer100g(bison, fat, 8.88).
+HasNutrient(bison, protein).
+NutrientValuePer100g(bison, protein, 19.8813).
+HasNutrient(blueberry, carbs).
+NutrientValuePer100g(blueberry, carbs, 14.5718).
+HasNutrient(blueberry, fat).
+NutrientValuePer100g(blueberry, fat, 0.3063).
+HasNutrient(blueberry, protein).
+NutrientValuePer100g(blueberry, protein, 0.703125).
+HasNutrient(broccoli, calories).
+NutrientValuePer100g(broccoli, calories, 81.75).
+HasNutrient(broccoli, carbs).
+NutrientValuePer100g(broccoli, carbs, 6.28).
+HasNutrient(broccoli, fat).
+NutrientValuePer100g(broccoli, fat, 0.34).
+HasNutrient(broccoli, protein).
+NutrientValuePer100g(broccoli, protein, 2.57).
+HasNutrient(butter, calories).
+NutrientValuePer100g(butter, calories, 1548.5).
+HasNutrient(butter, carbs).
+NutrientValuePer100g(butter, carbs, 20.1049).
+HasNutrient(butter, fat).
+NutrientValuePer100g(butter, fat, 63.2783).
+HasNutrient(butter, protein).
+NutrientValuePer100g(butter, protein, 21.746).
+HasNutrient(chicken, calories).
+NutrientValuePer100g(chicken, calories, 417.25).
+HasNutrient(chicken, carbs).
+NutrientValuePer100g(chicken, carbs, -0.170511).
+HasNutrient(chicken, fat).
+NutrientValuePer100g(chicken, fat, 6.76756).
+HasNutrient(chicken, protein).
+NutrientValuePer100g(chicken, protein, 21.1472).
+HasNutrient(egg, calories).
+NutrientValuePer100g(egg, calories, 759.556).
+HasNutrient(egg, carbs).
+NutrientValuePer100g(egg, carbs, 1.72667).
+HasNutrient(egg, fat).
+NutrientValuePer100g(egg, fat, 18.9189).
+HasNutrient(egg, protein).
+NutrientValuePer100g(egg, protein, 26.6111).
+HasNutrient(eggplant, carbs).
+NutrientValuePer100g(eggplant, carbs, 5.39933).
+HasNutrient(eggplant, fat).
+NutrientValuePer100g(eggplant, fat, 0.12).
+HasNutrient(eggplant, protein).
+NutrientValuePer100g(eggplant, protein, 0.851875).
+HasNutrient(flour, calories).
+NutrientValuePer100g(flour, calories, 961.394).
+HasNutrient(flour, carbs).
+NutrientValuePer100g(flour, carbs, 70.0841).
+HasNutrient(flour, fat).
+NutrientValuePer100g(flour, fat, 5.30004).
+HasNutrient(flour, protein).
+NutrientValuePer100g(flour, protein, 12.9619).
+HasNutrient(garlic, calories).
+NutrientValuePer100g(garlic, calories, 370).
+HasNutrient(garlic, carbs).
+NutrientValuePer100g(garlic, carbs, 28.2).
+HasNutrient(garlic, fat).
+NutrientValuePer100g(garlic, fat, 0.38).
+HasNutrient(garlic, protein).
+NutrientValuePer100g(garlic, protein, 6.62).
+HasNutrient(kale, calories).
+NutrientValuePer100g(kale, calories, 92.5).
+HasNutrient(kale, carbs).
+NutrientValuePer100g(kale, carbs, 4.86).
+HasNutrient(kale, fat).
+NutrientValuePer100g(kale, fat, 1.35).
+HasNutrient(kale, protein).
+NutrientValuePer100g(kale, protein, 2.93).
+HasNutrient(kiwi, calories).
+NutrientValuePer100g(kiwi, calories, 149.5).
+HasNutrient(kiwi, carbs).
+NutrientValuePer100g(kiwi, carbs, 13.909).
+HasNutrient(kiwi, fat).
+NutrientValuePer100g(kiwi, fat, 0.54).
+HasNutrient(kiwi, protein).
+NutrientValuePer100g(kiwi, protein, 1.03406).
+HasNutrient(lentils, carbs).
+NutrientValuePer100g(lentils, carbs, 62.1713).
+HasNutrient(lentils, fat).
+NutrientValuePer100g(lentils, fat, 1.925).
+HasNutrient(lentils, protein).
+NutrientValuePer100g(lentils, protein, 23.5688).
+HasNutrient(melon, calories).
+NutrientValuePer100g(melon, calories, 87.5).
+HasNutrient(melon, carbs).
+NutrientValuePer100g(melon, carbs, 8.16).
+HasNutrient(melon, fat).
+NutrientValuePer100g(melon, fat, 0.18).
+HasNutrient(melon, protein).
+NutrientValuePer100g(melon, protein, 0.82).
+HasNutrient(milk, calories).
+NutrientValuePer100g(milk, calories, 121.25).
+HasNutrient(milk, carbs).
+NutrientValuePer100g(milk, carbs, 3.73499).
+HasNutrient(milk, fat).
+NutrientValuePer100g(milk, fat, 2.95411).
+HasNutrient(milk, protein).
+NutrientValuePer100g(milk, protein, 3.97911).
+HasNutrient(mozzarella, calories).
+NutrientValuePer100g(mozzarella, calories, 769).
+HasNutrient(mozzarella, carbs).
+NutrientValuePer100g(mozzarella, carbs, 4.44).
+HasNutrient(mozzarella, fat).
+NutrientValuePer100g(mozzarella, fat, 20.4).
+HasNutrient(mozzarella, protein).
+NutrientValuePer100g(mozzarella, protein, 23.7).
+HasNutrient(mushroom, carbs).
+NutrientValuePer100g(mushroom, carbs, 6.53361).
+HasNutrient(mushroom, fat).
+NutrientValuePer100g(mushroom, fat, 0.268967).
+HasNutrient(mushroom, protein).
+NutrientValuePer100g(mushroom, protein, 2.66383).
+HasNutrient(olive_oil, carbs).
+NutrientValuePer100g(olive_oil, carbs, 2.4095).
+HasNutrient(olive_oil, fat).
+NutrientValuePer100g(olive_oil, fat, 9.853).
+HasNutrient(olive_oil, protein).
+NutrientValuePer100g(olive_oil, protein, 26.9375).
+HasNutrient(onion, calories).
+NutrientValuePer100g(onion, calories, 263.625).
+HasNutrient(onion, carbs).
+NutrientValuePer100g(onion, carbs, 15.63).
+HasNutrient(onion, fat).
+NutrientValuePer100g(onion, fat, 3.67).
+HasNutrient(onion, protein).
+NutrientValuePer100g(onion, protein, 1.56975).
+HasNutrient(parmesan_cheese, calories).
+NutrientValuePer100g(parmesan_cheese, calories, 1090.5).
+HasNutrient(parmesan_cheese, carbs).
+NutrientValuePer100g(parmesan_cheese, carbs, 8.36594).
+HasNutrient(parmesan_cheese, fat).
+NutrientValuePer100g(parmesan_cheese, fat, 28.75).
+HasNutrient(parmesan_cheese, protein).
+NutrientValuePer100g(parmesan_cheese, protein, 29.8696).
+HasNutrient(pasta, calories).
+NutrientValuePer100g(pasta, calories, 115.5).
+HasNutrient(pasta, carbs).
+NutrientValuePer100g(pasta, carbs, 8.05).
+HasNutrient(pasta, fat).
+NutrientValuePer100g(pasta, fat, 1.48).
+HasNutrient(pasta, protein).
+NutrientValuePer100g(pasta, protein, 1.10094).
+HasNutrient(peach, calories).
+NutrientValuePer100g(peach, calories, 108).
+HasNutrient(peach, carbs).
+NutrientValuePer100g(peach, carbs, 10.1).
+HasNutrient(peach, fat).
+NutrientValuePer100g(peach, fat, 0.27).
+HasNutrient(peach, protein).
+NutrientValuePer100g(peach, protein, 0.91).
+HasNutrient(pecans, carbs).
+NutrientValuePer100g(pecans, carbs, 12.6993).
+HasNutrient(pecans, fat).
+NutrientValuePer100g(pecans, fat, 73.28).
+HasNutrient(pecans, protein).
+NutrientValuePer100g(pecans, protein, 9.9587).
+HasNutrient(pine_nuts, carbs).
+NutrientValuePer100g(pine_nuts, carbs, 18.5921).
+HasNutrient(pine_nuts, fat).
+NutrientValuePer100g(pine_nuts, fat, 61.27).
+HasNutrient(pine_nuts, protein).
+NutrientValuePer100g(pine_nuts, protein, 15.7039).
+HasNutrient(pork, calories).
+NutrientValuePer100g(pork, calories, 820.5).
+HasNutrient(pork, carbs).
+NutrientValuePer100g(pork, carbs, 5.93788).
+HasNutrient(pork, fat).
+NutrientValuePer100g(pork, fat, 19.5002).
+HasNutrient(pork, protein).
+NutrientValuePer100g(pork, protein, 18.4276).
+HasNutrient(ricotta, calories).
+NutrientValuePer100g(ricotta, calories, 407).
+HasNutrient(ricotta, carbs).
+NutrientValuePer100g(ricotta, carbs, 6.86).
+HasNutrient(ricotta, fat).
+NutrientValuePer100g(ricotta, fat, 11).
+HasNutrient(ricotta, protein).
+NutrientValuePer100g(ricotta, protein, 7.81).
+HasNutrient(salmon, carbs).
+NutrientValuePer100g(salmon, carbs, 0).
+HasNutrient(salmon, fat).
+NutrientValuePer100g(salmon, fat, 9.023).
+HasNutrient(salmon, protein).
+NutrientValuePer100g(salmon, protein, 21.3094).
+HasNutrient(salt, calories).
+NutrientValuePer100g(salt, calories, 1586).
+HasNutrient(salt, carbs).
+NutrientValuePer100g(salt, carbs, 17.1).
+HasNutrient(salt, fat).
+NutrientValuePer100g(salt, fat, 56.1).
+HasNutrient(salt, protein).
+NutrientValuePer100g(salt, protein, 21).
+HasNutrient(snapper, carbs).
+NutrientValuePer100g(snapper, carbs, 0.4413).
+HasNutrient(snapper, fat).
+NutrientValuePer100g(snapper, fat, 0.5692).
+HasNutrient(snapper, protein).
+NutrientValuePer100g(snapper, protein, 20.6625).
+HasNutrient(spinach, carbs).
+NutrientValuePer100g(spinach, carbs, 2.52384).
+HasNutrient(spinach, fat).
+NutrientValuePer100g(spinach, fat, 0.6116).
+HasNutrient(spinach, protein).
+NutrientValuePer100g(spinach, protein, 2.87906).
+HasNutrient(sugar, calories).
+NutrientValuePer100g(sugar, calories, 997.5).
+HasNutrient(sugar, carbs).
+NutrientValuePer100g(sugar, carbs, 70.6359).
+HasNutrient(sugar, fat).
+NutrientValuePer100g(sugar, fat, 0.597667).
+HasNutrient(sugar, protein).
+NutrientValuePer100g(sugar, protein, 1.57812).
+HasNutrient(swordfish, carbs).
+NutrientValuePer100g(swordfish, carbs, 0.4495).
+HasNutrient(swordfish, fat).
+NutrientValuePer100g(swordfish, fat, 8.123).
+HasNutrient(swordfish, protein).
+NutrientValuePer100g(swordfish, protein, 19.2375).
+HasNutrient(tomato, calories).
+NutrientValuePer100g(tomato, calories, 58.5).
+HasNutrient(tomato, carbs).
+NutrientValuePer100g(tomato, carbs, 6.68066).
+HasNutrient(tomato, fat).
+NutrientValuePer100g(tomato, fat, 0.42513).
+HasNutrient(tomato, protein).
+NutrientValuePer100g(tomato, protein, 1.339).
+HasNutrient(tuna, calories).
+NutrientValuePer100g(tuna, calories, 233).
+HasNutrient(tuna, carbs).
+NutrientValuePer100g(tuna, carbs, -0.01225).
+HasNutrient(tuna, fat).
+NutrientValuePer100g(tuna, fat, 0.66375).
+HasNutrient(tuna, protein).
+NutrientValuePer100g(tuna, protein, 21.85).
+HasNutrient(walnuts, carbs).
+NutrientValuePer100g(walnuts, carbs, 10.9096).
+HasNutrient(walnuts, fat).
+NutrientValuePer100g(walnuts, fat, 69.74).
+HasNutrient(walnuts, protein).
+NutrientValuePer100g(walnuts, protein, 14.5644).
+HasNutrient(yogurt, calories).
+NutrientValuePer100g(yogurt, calories, 186.75).
+HasNutrient(yogurt, carbs).
+NutrientValuePer100g(yogurt, carbs, 7.97225).
+HasNutrient(yogurt, fat).
+NutrientValuePer100g(yogurt, fat, 0.2025).
+HasNutrient(yogurt, protein).
+NutrientValuePer100g(yogurt, protein, 7.52892).
+HasNutrient(zucchini, carbs).
+NutrientValuePer100g(zucchini, carbs, 3.27).
+HasNutrient(zucchini, fat).
+NutrientValuePer100g(zucchini, fat, 0.205).
+HasNutrient(zucchini, protein).
+NutrientValuePer100g(zucchini, protein, 0.984375).
